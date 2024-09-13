7f752d7edc860b2943182a7ade34abc69189b59c
Kumarsantanu2002 <158434127+Kumarsantanu2002@users.noreply.github.com>
2024-09-13 11:51:31 +0530

    Create 169. Majority Element.java

---
+++ 169. Majority Element.java
@@ -0,0 +1,24 @@
+// 169. Majority Element
+
+ public int majorityElement(int[] nums) {
+        int count=0;
+        int ans=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(count==0)
+            {
+                ans=nums[i];
+                count=1;
+            }
+            else{
+                if(ans==nums[i])
+                {
+                    count++;
+                }
+                else{
+                    count--;
+                }
+            }
+        }
+        return ans;
+    }
